e538b04626c9e6b730a4dad3f043c40802c94fd3
Lakhan Raut <lakhanraut709@gmail.com>
2025-07-21 22:37:49 +0530

    FourthDay-commit

---
+++ src/customer/Data/mens_kurta.js
@@ -0,0 +1,35 @@
+
+export const mens_kurta = [
+  {
+    id: 1,
+    name: "Cotton Printed Kurta",
+    price: 999,
+    image: "https://example.com/image1.jpg",
+    category: "men",
+    description: "Soft cotton kurta perfect for summer wear."
+  },
+  {
+    id: 2,
+    name: "Silk Festive Kurta",
+    price: 1499,
+    image: "https://example.com/image2.jpg",
+    category: "men",
+    description: "Ideal for weddings and traditional events."
+  },
+  {
+    id: 3,
+    name: "Casual Linen Kurta",
+    price: 799,
+    image: "https://example.com/image3.jpg",
+    category: "men",
+    description: "Simple and elegant linen kurta for daily use."
+  },
+    {
+    id: 4,
+    name: "Casual Linen Kurta",
+    price: 799,
+    image: "https://example.com/image3.jpg",
+    category: "men",
+    description: "Simple and elegant linen kurta for daily use."
+  }
+];
