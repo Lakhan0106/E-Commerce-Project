8dde3890ed662e2965183e86c03a8e37e0ce20e5
Lakhan Raut <lakhanraut709@gmail.com>
2025-07-22 21:52:40 +0530

    FifthDay-commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 import Navigation from './customer/components/Navigation/Navigation.jsx';
 import HomePage from './customer/Pages/Homepage/HomePage.jsx';
 import Footer from './customer/components/Footer/Footer.jsx';
-import Product from './customer/components/Product.jsx'
+// import Product from './customer/components/Product/Product.jsx'
 
 
 
 function App() {
   return (
     <div className="">
-    <Navigation/>
-    <div> 
-      {/* <HomePage/> */}
-      <Product/>
-    </div>
-    <Footer/>
+      <Navigation />
+      <div>
+        <HomePage/>
+
+      </div>
+      <Footer />
     </div>
   );
 }
