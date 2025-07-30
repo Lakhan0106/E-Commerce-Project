5e65cd3c5d9b3f0712af97777eba44101bdfd9b7
Lakhan Raut <lakhanraut709@gmail.com>
2025-07-30 22:23:05 +0530

    CartPage

--- src/customer/components/Cart/Cart.jsx
+++ src/customer/components/Cart/Cart.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import CartItems from './CartItems'
+import { Button } from '@mui/material'
 
 const Cart = () => {
   return (
     <div>
-      <CartItems/>
+
+      <div className='lg:grid grid-cols-3 lg:px-16 relative'>
+        <div className='col-span-2'>
+          {[1,1,1,1,1,1].map((item)=><CartItems />)}
+        </div>
+        <div className='px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0'>
+          <div className='border'>
+            <p className='uppercase font-bold opacity-60 pb-4 '>Price Details</p>
+            <hr />
+            <div className='space-y-3 font-semobold mb-10' >
+              <div className='flex justify-between pt-3 text-black'>
+                <span>Price</span>
+                <span>Rs.2300</span>
+
+              </div>
+              <div className='flex justify-between pt-3'>
+                <span>Disccount</span>
+                <span className='text-green-600'>-1000</span>
+
+              </div>
+              <div className='flex justify-between pt-3'>
+                <span>Delivary Charges</span>
+                <span className='text-green-600'>Free</span>
+
+              </div>
+              <div className='flex justify-between pt-3 font-bold '>
+                <span>Total Amount</span>
+                <span className='text-green-600'>Rs.1300</span>
+
+              </div>
+            </div>
+            <Button color="secondary" variant="contained" className='w-full mt-5' sx={{ px: "2.5rem", py: ".7rem" }}>
+              Checkout
+            </Button>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
     </div>
   )
 }
